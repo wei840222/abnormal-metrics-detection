338807a06a6642644c173b786fcbd8dda9611b2e
wei840222 <wei840222@gmail.com>
2022-10-15 01:20:31 +0800

    first api

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # abnormal-metrics-detection
+API for detecte abnormal metrics
+
+## How to build?
+```bash
+pack build --builder=gcr.io/buildpacks/builder:v1 --publish wei840222/amd:1 --env GOOGLE_ENTRYPOINT="uvicorn main:app --host=0.0.0.0 --port=8080"
+```
+
+## How to deploy?
+```bash
+kn ksvc apply --image=wei840222/amd:1 amd
+```
---
+++ main.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI, UploadFile, HTTPException
+import pandas as pd
+from adtk.data import validate_series
+from adtk.detector import LevelShiftAD
+from adtk.visualization import plot
+from matplotlib import pyplot as plt
+from io import BytesIO
+from starlette.responses import StreamingResponse
+
+app = FastAPI()
+
+@app.get("/")
+async def ping():
+    return "pong"
+
+@app.post("/adtk/level-shift-ad")
+async def adtk_level_shift_ad(data: UploadFile):
+    match data.content_type:
+        case "text/csv":
+            s_train = pd.read_csv(data.file, index_col="timestamp", parse_dates=True).squeeze()
+        case _:
+            raise HTTPException(status_code=400, detail="File type not supported")
+    
+    s_train = validate_series(s_train)
+    level_shift_ad = LevelShiftAD(c=6.0, side='both', window=200)
+    anomalies = level_shift_ad.fit_detect(s_train)
+    plot(s_train, anomaly=anomalies, ts_linewidth=1, ts_markersize=1, anomaly_color='red')
+    buf = BytesIO()
+    plt.gcf().savefig(buf, format="png")
+    buf.seek(0)
+    return StreamingResponse(buf, media_type="image/png")
