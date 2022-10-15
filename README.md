9828ffa169d3b0fac6b5a53f3fcfe5b88a59014b
wei840222 <wei840222@gmail.com>
2022-10-15 13:29:19 +0800

    add log

--- README.md
+++ README.md
@@ -9,10 +9,10 @@ pipenv install
 ## How to build?
 ```bash
 pipenv lock -r > requirements.txt
-pack build --builder=gcr.io/buildpacks/builder:v1 --publish wei840222/amd:2 --env GOOGLE_ENTRYPOINT="uvicorn main:app --host=0.0.0.0 --port=8080"
+pack build --builder=gcr.io/buildpacks/builder:v1 --publish wei840222/amd:3 --env GOOGLE_ENTRYPOINT="uvicorn main:app --host=0.0.0.0 --port=8080 --workers=4 --log-level=debug"
 ```
 
 ## How to deploy?
 ```bash
-kn ksvc apply --image=wei840222/amd:2 amd
+kn ksvc apply --image=wei840222/amd:3 amd
 ```
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import logging
 from fastapi import FastAPI, UploadFile, HTTPException
 import pandas as pd
 from adtk.data import validate_series
@@ -7,6 +8,7 @@ from matplotlib import pyplot as plt
 from io import BytesIO
 from starlette.responses import StreamingResponse
 
+logger = logging.getLogger("uvicorn.error")
 app = FastAPI()
 
 @app.get("/")
@@ -15,6 +17,7 @@ async def ping():
 
 @app.post("/adtk/level-shift-ad")
 async def adtk_level_shift_ad(data: UploadFile):
+    logger.debug(f"filename: {data.filename}, content_type: {data.content_type}")
     match data.content_type:
         case "text/csv":
             s_train = pd.read_csv(data.file, index_col="timestamp", parse_dates=True).squeeze()
@@ -22,10 +25,17 @@ async def adtk_level_shift_ad(data: UploadFile):
             raise HTTPException(status_code=400, detail="File type not supported")
     
     s_train = validate_series(s_train)
+    logger.debug(f"request data\n{s_train}")
+
     level_shift_ad = LevelShiftAD(c=6.0, side='both', window=200)
-    anomalies = level_shift_ad.fit_detect(s_train)
-    plot(s_train, anomaly=anomalies, ts_linewidth=1, ts_markersize=1, anomaly_color='red')
+
+    anomalies = level_shift_ad.fit_detect(s_train, return_list=True)
+    logger.debug(f"anomalies: {anomalies}")
+
+    fig, ax = plt.subplots(nrows=1, figsize=(16, 4 * 1), sharex=True)
+    plot(s_train, anomaly=anomalies, ts_linewidth=1, ts_markersize=1, anomaly_color='red', axes=ax)
+
     buf = BytesIO()
-    plt.gcf().savefig(buf, format="png")
+    fig.savefig(buf, format="png")
     buf.seek(0)
     return StreamingResponse(buf, media_type="image/png")
